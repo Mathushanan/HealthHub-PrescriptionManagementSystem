f4074c353f8c645b658c92b2aff23576ec1eebb4
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2023-11-15 00:01:31 +0530

    Registration page structure created

---
+++ register.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HealthHub</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="box form-box register-box">
+            <header>Register</header>
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" required>
+                </div>
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" required>
+                </div>
+                <div class="field input">
+                    <label for="mobile">Mobile</label>
+                    <input type="text" name="mobile" id="mobile" required>
+                </div>
+                <div class="field input">
+                    <label for="address">Address</label>
+                    <input type="text" name="address" id="address" required>
+                </div>
+                <div class="field input">
+                    <label for="dob">Date of Birth</label>
+                    <input type="date" name="dob" id="dob" required>
+                </div>
+                <div class="field input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" required>
+                </div>
+                <div class="field input">
+                    <label for="cpassword">Confirm Password</label>
+                    <input type="password" name="cpassword" id="cpassword" required>
+                </div>
+                <div class="field">
+                    <input type="submit" class="btn" name="submit" value="SIGNUP">
+                </div>
+                <div class="link">
+                    Already have an Account? <a href="index.php">LOGIN</a>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+    
+</body>
+</html>
