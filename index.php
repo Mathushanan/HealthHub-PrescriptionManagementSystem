e96b476ccbebd00d9cc0846dc931987d33890971
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2023-11-17 21:02:49 +0530

    Type based user login implemented

--- index.php
+++ index.php
@@ -5,19 +5,21 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HealthHub</title>
     <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
 </head>
+
 <body>
     <div class="nav">
         <div class="logo">
             <p>Health<span class="sub-logo">Hub</span></p>
         </div>
         <div class="right-links">
-            
+
         </div>
     </div>
     <div class="container">
@@ -25,27 +27,32 @@ session_start();
             <?php
 
             include("config.php");
-            if(isset($_POST["submit"])){
+            if (isset($_POST["submit"])) {
 
-                $email=mysqli_real_escape_string($connection,$_POST['email']);
-                $password=mysqli_real_escape_string($connection,$_POST['password']);
-                $result=mysqli_query($connection,"SELECT * FROM users WHERE email='$email' AND password='$password'");
-                
+                $email = mysqli_real_escape_string($connection, $_POST['email']);
+                $password = mysqli_real_escape_string($connection, $_POST['password']);
+                $result = mysqli_query($connection, "SELECT * FROM users WHERE email='$email' AND password='$password'");
 
-                if($result && mysqli_num_rows($result) > 0){
 
-                    $row=mysqli_fetch_assoc($result);
-                    $_SESSION['email']=$row['email'];
-                    $_SESSION['name']=$row['name'];
-                    $_SESSION['password']=$row['password'];
-                    $_SESSION['dob']=$row['dob'];
-                    $_SESSION['mobile']=$row['mobile'];
-                    $_SESSION['address']=$row['address'];
+                if ($result && mysqli_num_rows($result) > 0) {
 
-                    header("Location:uploadPrescription.php");
-                    exit();
-                    
-                }else{
+                    $row = mysqli_fetch_assoc($result);
+                    $_SESSION['email'] = $row['email'];
+                    $_SESSION['name'] = $row['name'];
+                    $_SESSION['password'] = $row['password'];
+                    $_SESSION['dob'] = $row['dob'];
+                    $_SESSION['mobile'] = $row['mobile'];
+                    $_SESSION['address'] = $row['address'];
+                    $_SESSION['type'] = $row['type'];
+
+                    if ($_SESSION['type'] == "Parmacy User") {
+                        header("Location:viewPrescription.php");
+                        exit();
+                    } else {
+                        header("Location:uploadPrescription.php");
+                        exit();
+                    }
+                } else {
                     echo "
                          <div class='ErrorMessageBox'>
                             <p>Wrong Email or Password!</p>
@@ -53,34 +60,33 @@ session_start();
                     echo "
                         <a href='index.php'><button class='btn'>GO BACK</button></a>
                     ";
-                   
                 }
-                
-            }else{
+            } else {
 
-            
+
             ?>
-            <header>Login</header>
-            <form action="" method="post">
-                <div class="field input">
-                    <label for="email">Email</label>
-                    <input type="text" name="email" id="email" required>
-                </div>
-                <div class="field input">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" required>
-                </div>
-                <div class="field">
-                    <input type="submit" class="btn" name="submit" value="LOGIN">
-                </div>
-                <div class="link">
-                    Don't have Account? <a href="register.php">SIGNUP</a>
-                </div>
-            </form>
+                <header>Login</header>
+                <form action="" method="post">
+                    <div class="field input">
+                        <label for="email">Email</label>
+                        <input type="text" name="email" id="email" required>
+                    </div>
+                    <div class="field input">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" required>
+                    </div>
+                    <div class="field">
+                        <input type="submit" class="btn" name="submit" value="LOGIN">
+                    </div>
+                    <div class="link">
+                        Don't have Account? <a href="register.php">SIGNUP</a>
+                    </div>
+                </form>
         </div>
-        <?php } ?>
+    <?php } ?>
     </div>
-    
-    
+
+
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -1,103 +1,110 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HealthHub</title>
     <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
 </head>
+
 <body>
     <div class="nav">
         <div class="logo">
             <p>Health<span class="sub-logo">Hub</span></p>
         </div>
         <div class="right-links">
-            
+
         </div>
     </div>
     <div class="container">
         <div class="box form-box register-box">
 
-        <?php
-        include("config.php");
-        if(isset($_POST['submit'])){
+            <?php
+            include("config.php");
+            if (isset($_POST['submit'])) {
 
-            $name=$_POST['name'];
-            $email=$_POST['email'];
-            $mobile=$_POST['mobile'];
-            $address=$_POST['address'];
-            $dob=$_POST['dob'];
-            $password=$_POST['password'];
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $mobile = $_POST['mobile'];
+                $address = $_POST['address'];
+                $dob = $_POST['dob'];
+                $password = $_POST['password'];
+                $type=$_POST['type'];
 
-            $verifyQuery=mysqli_query($connection,"SELECT * FROM users WHERE email='$email'");
+                $verifyQuery = mysqli_query($connection, "SELECT * FROM users WHERE email='$email'");
 
-            if(mysqli_num_rows($verifyQuery)!=0){
+                if (mysqli_num_rows($verifyQuery) != 0) {
 
-                echo "
+                    echo "
                 <div class='ErrorMessageBox'>
                     <p>Email is already registered!</p>
                 <div><br>";
-                echo "
+                    echo "
                 <a href='register.php'><button class='btn'>GO BACK</button></a>
                 ";
-
-            }else{
-                mysqli_query($connection,"INSERT INTO users (name,email,address,mobile,dob,password) VALUES ('$name','$email','$address','$mobile','$dob','$password')");
-                echo "
+                } else {
+                    mysqli_query($connection, "INSERT INTO users (name,email,address,mobile,dob,password,type) VALUES ('$name','$email','$address','$mobile','$dob','$password','$type')");
+                    echo "
                 <div class='SuccessMessageBox'>
                     <p>Registration successfull!</p>
                 <div><br>";
-                echo "
+                    echo "
                 <a href='index.php'><button class='btn'>LOGIN</button></a>
                 ";
+                }
+            } else {
 
-            }
-
-        
-        }else{
-
-        ?>
-            <header>Register</header>
-            <form action="" method="post">
-                <div class="field input">
-                    <label for="name">Name</label>
-                    <input type="text" name="name" id="name" required>
-                </div>
-                <div class="field input">
-                    <label for="email">Email</label>
-                    <input type="text" name="email" id="email" required>
-                </div>
-                <div class="field input">
-                    <label for="mobile">Mobile</label>
-                    <input type="text" name="mobile" id="mobile" required>
-                </div>
-                <div class="field input">
-                    <label for="address">Address</label>
-                    <input type="text" name="address" id="address" required>
-                </div>
-                <div class="field input">
-                    <label for="dob">Date of Birth</label>
-                    <input type="date" name="dob" id="dob" required>
-                </div>
-                <div class="field input">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" required>
-                </div>
-                <div class="field input">
-                    <label for="cpassword">Confirm Password</label>
-                    <input type="password" name="cpassword" id="cpassword" required>
-                </div>
-                <div class="field">
-                    <input type="submit" class="btn" name="submit" value="SIGNUP">
-                </div>
-                <div class="link">
-                    Already have an Account? <a href="index.php">LOGIN</a>
-                </div>
-            </form>
+            ?>
+                <header>Register</header>
+                <form action="" method="post">
+                    <div class="field input">
+                       
+                        <select id="type" name="type">
+                            <option value="Parmacy User">Parmacy User</option>
+                            <option value="Normal User">Normal User</option>
+                        </select>
+                    </div>
+                    <div class="field input">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" required>
+                    </div>
+                    <div class="field input">
+                        <label for="email">Email</label>
+                        <input type="text" name="email" id="email" required>
+                    </div>
+                    <div class="field input">
+                        <label for="mobile">Mobile</label>
+                        <input type="text" name="mobile" id="mobile" required>
+                    </div>
+                    <div class="field input">
+                        <label for="address">Address</label>
+                        <input type="text" name="address" id="address" required>
+                    </div>
+                    <div class="field input">
+                        <label for="dob">Date of Birth</label>
+                        <input type="date" name="dob" id="dob" required>
+                    </div>
+                    <div class="field input">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" required>
+                    </div>
+                    <div class="field input">
+                        <label for="cpassword">Confirm Password</label>
+                        <input type="password" name="cpassword" id="cpassword" required>
+                    </div>
+                    <div class="field">
+                        <input type="submit" class="btn" name="submit" value="SIGNUP">
+                    </div>
+                    <div class="link">
+                        Already have an Account? <a href="index.php">LOGIN</a>
+                    </div>
+                </form>
         </div>
-        <?php } ?>
+    <?php } ?>
     </div>
-    
-    
+
+
 </body>
+
 </html>
